cd1d46ffb344e76d52371c53e82027966edc6d5d
董文杰 <dongwenjie@dongwenjiedeMacBook-Pro.local>
2019-06-20 20:33:14 +0800

    xxx

--- _http.js
+++ _http.js
@@ -1,5 +1,5 @@
 // 1.引入http模块
-const http =require('http');
+const http = require('http');
 
 // 2.用http模块创建服务
 /**
@@ -7,16 +7,15 @@ const http =require('http');
  * res 返回相应信息  （response）
  */
 
- http.createServer((req,res)=>{
+http.createServer((req, res) => {
     // 设置http头部，状态码是200， 文件类型是html，字符集是UTF8
     res.writeHead(200, {
-         "Content-type":"text/html;charset:UTF8"
+        "Content-type": "text/html;charset:UTF8"
     })
 
     // 往页面打印
     res.write("<h1>My name is Dongwenjie</h1>");
-     
+
     // 结束响应
     res.end();
- }).listen(3000)
-
+}).listen(4000)
